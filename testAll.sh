11cdf49549ada2447d02f4fb91c863ae1feef58a
Jye Lewis <jlewis@noggin.com.au>
2017-12-21 20:50:33 +1100

    Debugging travis script

--- testAll.sh
+++ testAll.sh
@@ -16,6 +16,11 @@ cd e2eTests
 yarn
 cd ..
 
+pwd
+ls
+echo $(find ./server/src -type f \( -name "*.test.js" -o -name "*.itest.js" \)) \
+    $(find ./websocketClient/src -type f \( -name "*.test.js" -o -name "*.itest.js" \))
+
 #run all tests (with coverage)
 ./server/node_modules/nyc/bin/nyc.js ./server/node_modules/ava/cli.js --verbose \
     $(find ./server/src -type f \( -name "*.test.js" -o -name "*.itest.js" \)) \
