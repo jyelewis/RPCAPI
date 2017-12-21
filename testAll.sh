f1981c4c0b3e5475c2f2c302bc3fcfa76e61a4f5
Jye Lewis <jlewis@noggin.com.au>
2017-12-21 20:42:47 +1100

    Use find to get all .test files

--- testAll.sh
+++ testAll.sh
@@ -17,7 +17,9 @@ yarn
 cd ..
 
 #run all tests (with coverage)
-./server/node_modules/nyc/bin/nyc.js ./server/node_modules/ava/cli.js --verbose ./server/**/src/**/*.test.js ./server/**/src/**/*.itest.js ./websocketClient/**/src/**/*.test.js ./websocketClient/**/src/**/*.itest.js
+./server/node_modules/nyc/bin/nyc.js ./server/node_modules/ava/cli.js --verbose \
+    $(find ./server/src -type f \( -name "*.test.js" -o -name "*.itest.js" \)) \
+    $(find ./websocketClient/src -type f \( -name "*.test.js" -o -name "*.itest.js" \))
 
 #e2e tests
 cd e2eTests
